539a965141171d7118366cc88fab8197da1cfa5b
Dan Korostelev <nadako@gmail.com>
2016-10-21 04:20:16 +0300

    start teh bot

--- src/Main.hx
+++ src/Main.hx
@@ -16,15 +16,18 @@ class Main {
                 });
             }
         }
+        new PollUpdateListener(api, onUpdate, 1000).start();
 
-        var server = js.node.Http.createServer(function(req, res) {
-            trace(req);
-            res.end("hi");
-        });
+        // var server = js.node.Http.createServer(function(req, res) {
+        //     req.on("data", function(data) {
+        //         trace(data);
+        //     });
+        //     res.end("hi");
+        // });
 
-        var port = Sys.getEnv("PORT");
-        var port = if (port == null) 80 else Std.parseInt(port);
-        server.listen(port);
+        // var port = Sys.getEnv("PORT");
+        // var port = if (port == null) 80 else Std.parseInt(port);
+        // server.listen(port);
 
         // var listener = new WebhookUpdateListener(api, onUpdate, "https://95.55.124.126:8443/");
         // listener.start(function() {
